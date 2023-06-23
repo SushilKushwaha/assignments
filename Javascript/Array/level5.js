df977b799073880e37e95231905341b9afaa63f2
kaylinkhanal10 <kyalinnop@gmail.com>
2023-06-23 09:22:49 +0545

    added level 5 array

--- Javascript/Array/level5.js
+++ Javascript/Array/level5.js
@@ -1,8 +1,12 @@
 const arr = [2,5,6,7]
 //calculate the sum of the array using either for of or for in
 
+
+
 //high level
 const users =['hari', 'shyam', 'hari']
+//hint: how to know hari was already there in users array
+//hint2: how to remove particular index/item
 //remove from array if duplicate
 
 const newArr = [[4,5], [5,7], [7,2]]
