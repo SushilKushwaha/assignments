1717b5e115e3660a8586c146743d0a9aaa180213
kaylinkhanal10 <kyalinnop@gmail.com>
2023-06-23 09:18:53 +0545

    added level 5 array

---
+++ Javascript/Array/level5.js
@@ -0,0 +1,9 @@
+const arr = [2,5,6,7]
+//calculate the sum of the array using either for of or for in
+
+//high level
+const users =['hari', 'shyam', 'hari']
+//remove from array if duplicate
+
+const newArr = [[4,5], [5,7], [7,2]]
+//calculate the sum of all the odd numbers inside this nested array
